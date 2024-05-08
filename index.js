30e02fc00ddb41972e3a1a91ea67b7c157c796cd
maianhbao112 <fafasdfadspun0suy@hotmail.com>
2024-05-08 15:08:20 +0700

    start project

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function pathSum(root, sum) {
-  if (!root) return [];
-  const result = [];
-  const path = [];
-  const traverse = (node, sum) => {
-    if (!node) return;
-    path.push(node.val);
-    if (!node.left && !node.right && sum === node.val) {
-      result.push([...path]);
-    }
-    traverse(node.left, sum - node.val);
-    traverse(node.right, sum - node.val);
-    path.pop();
-  };
-  traverse(root, sum);
-  return result;
-}
+const intersection = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => s.has(x));
+};
