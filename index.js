52520a778fbdfae7e0318e2f0b3914f5ec1636b1
maianhbao112 <fafasdfadspun0suy@hotmail.com>
2024-05-12 12:02:26 +0700

    test project

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-function mergeKLists(lists) {
-  if (!lists || !lists.length) return null;
-  return mergeLists(lists, 0, lists.length - 1);
-  function mergeLists(lists, start, end) {
-    if (start === end) return lists[start];
-    const mid = Math.floor((start + end) / 2);
-    const left = mergeLists(lists, start, mid);
-    const right = mergeLists(lists, mid + 1, end);
-    return mergeTwoLists(left, right);
+function countSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    extendPalindrome(i, i);
+    extendPalindrome(i, i + 1);
   }
-  function mergeTwoLists(l1, l2) {
-    if (!l1) return l2;
-    if (!l2) return l1;
-    if (l1.val < l2.val) {
-      l1.next = mergeTwoLists(l1.next, l2);
-      return l1;
-    } else {
-      l2.next = mergeTwoLists(l1, l2.next);
-      return l2;
+  return count;
+  function extendPalindrome(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
     }
   }
 }
